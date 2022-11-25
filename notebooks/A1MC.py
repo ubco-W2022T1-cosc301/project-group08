6c4c37dfaaf88702202b1af9888a921c2ae07c65
ZhiHengZhang01 <77310385+ZhiHengZhang01@users.noreply.github.com>
2022-11-25 14:47:27 -0800

    Add files via upload

--- notebooks/A1MC.py
+++ notebooks/A1MC.py
@@ -14,10 +14,10 @@ def start(path_player,path_win_org):
 def process_player(player_info):
     clean = player_info[["player_name","nationality","player_status"]]
     player_info[["player_name","nationality","team","player_status"]]
-    active = clean[clean["player_status"]=="Active"]
-    retired = clean[clean["player_status"]=="Retired"]
+    active = clean[clean["player_status"]=="Active"].head()
+    retired = clean[clean["player_status"]=="Retired"].head()
     
-    print("Display of Players and their status")
+    print("Display of first 10 Players in list and their status")
     active_style = active.style.set_table_attributes("style='display:inline; margin-right:20px;'").set_caption("Active Player")
     retired_style = retired.style.set_table_attributes("style='display:inline'").set_caption("Retired Player")
     display_html(active_style._repr_html_() + retired_style._repr_html_(), raw=True)
@@ -42,47 +42,63 @@ def process_org(win_by_org):
             for earnings in win2022["earnings"]:
                 yearsum[i]+=earnings
     year_earn_compare = {"year":[2019,2020,2021,2022],"total":yearsum}
+    show2019(win2019,win2020,win2021,win2022,year_earn_compare)
     
+def show2019(win2019,win2020,win2021,win2022,year_earn_compare):
     print("Display of top 20 earnings in 2019")
     win2019_01 = win2019[win2019["earnings_rank"]<21]
     win2019_f20 = sns.barplot(y=win2019_01["earnings"],x=win2019_01["team"])
     win2019_f20.set(title="Earning of top 20 teams in 2019")
     plt.gcf().set_size_inches(40, 20)
     plt.show()
+    show2020(win2019,win2020,win2021,win2022,year_earn_compare)
     
+def show2020(win2019,win2020,win2021,win2022,year_earn_compare):
     print("Display of top 20 earnings in 2020")
     win2020_01 = win2020[win2020["earnings_rank"]<21]
     win2020_f20 = sns.barplot(y=win2020_01["earnings"],x=win2020_01["team"])
     win2020_f20.set(title="Earning of top 20 teams in 2020")
     plt.gcf().set_size_inches(40, 20)
     plt.show()
+    show2021(win2019,win2020,win2021,win2022,year_earn_compare)
     
+def show2021(win2019,win2020,win2021,win2022,year_earn_compare):
     print("Display of top 20 earnings in 2021")
     win2021_01 = win2021[win2021["earnings_rank"]<21]
     win2021_f20 = sns.barplot(y=win2021_01["earnings"],x=win2021_01["team"])
     win2021_f20.set(title="Earning of top 20 teams in 2021")
     plt.gcf().set_size_inches(40, 20)
     plt.show()
+    show2022(win2019,win2020,win2021,win2022,year_earn_compare)
     
+def show2022(win2019,win2020,win2021,win2022,year_earn_compare):
     print("Display of top 20 earnings in 2022")
     win2022_01 = win2022[win2022["earnings_rank"]<21]
     win2022_f20 = sns.barplot(y=win2022_01["earnings"],x=win2022_01["team"])
     win2022_f20.set(title="Earning of top 20 teams in 2022")
     plt.gcf().set_size_inches(40, 20)
     plt.show()
+    show_year_compare(win2019,win2020,win2021,win2022,year_earn_compare)
     
+def show_year_compare(win2019,win2020,win2021,win2022,year_earn_compare):
     print("Display of total year earning compare")
     year_comp = sns.barplot(x=year_earn_compare["year"],y=year_earn_compare["total"])
     year_comp.set(title="Earning of all teams each year",ylabel="earnings in 10^6")
     year_comp.bar_label(year_comp.containers[0])
     plt.gcf().set_size_inches(20, 5)
     plt.show()
+    show_top_5_by_year(win2019,win2020,win2021,win2022)
     
-    print("Display of all teams' ranking by year")
-    win2019_style = win2019.style.set_table_attributes("style='display:inline; margin-right:20px;'").set_caption("Earning Raank 2019")
-    win2020_style = win2020.style.set_table_attributes("style='display:inline; margin-right:20px;'").set_caption("Earning Raank 2020")
-    win2021_style = win2021.style.set_table_attributes("style='display:inline; margin-right:20px;'").set_caption("Earning Raank 2021")
-    win2022_style = win2022.style.set_table_attributes("style='display:inline'").set_caption("win2022")
+def show_top_5_by_year(win2019,win2020,win2021,win2022):
+    print("Display of top 5 teams' ranking by year")
+    win2019head=win2019.head()
+    win2020head=win2020.head()
+    win2021head=win2021.head()
+    win2022head=win2022.head()
+    win2019_style = win2019head.style.set_table_attributes("style='display:inline; margin-right:20px;'").set_caption("Earning Raank 2019")
+    win2020_style = win2020head.style.set_table_attributes("style='display:inline; margin-right:20px;'").set_caption("Earning Raank 2020")
+    win2021_style = win2021head.style.set_table_attributes("style='display:inline; margin-right:20px;'").set_caption("Earning Raank 2021")
+    win2022_style = win2022head.style.set_table_attributes("style='display:inline'").set_caption("win2022")
     display_html(win2019_style._repr_html_() + win2020_style._repr_html_()+ win2021_style._repr_html_()+ win2022_style._repr_html_(), raw=True)    
     
     
